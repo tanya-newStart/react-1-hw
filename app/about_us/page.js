47a1db7504a6e780e5f4035ec6213123a86c18d9
Tanya Dewland <tanya.dewland@gmail.com>
2024-08-27 13:52:24 +0200

    Add OurCrew.js, update image names

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,5 +1,6 @@
 import { OurValues } from "./OurValues";
 import { OurMission } from "./OurMission";
+import { OurCrew } from "./OurCrew";
 
 import styles from "./page.module.css";
 
@@ -7,15 +8,6 @@ import styles from "./page.module.css";
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
 // OurValues.js, OurCrew.js, OurPartners.js should live in this folder
 // import and use the components from the newly created files
-
-const OurCrew = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Crew section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Use the pictures from /public/crew
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return <p> ADD OUR CREW HERE </p>;
-};
 
 const OurPartners = () => {
   // TASK - React 1 week 1
@@ -30,7 +22,6 @@ export const Crew = () => {
   return (
     <div className="fullBGpicture">
       <main className="mainContent">
-        <h1>About us</h1>
         <section className="card">
           <h2>Our Mission</h2>
           <OurMission />
@@ -38,7 +29,6 @@ export const Crew = () => {
           <OurValues />
         </section>
         <section className="card">
-          <h2>The crew</h2>
           <OurCrew />
         </section>
 
