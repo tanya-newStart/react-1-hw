b1ab909dcc3fac60221a2d21ab359ee183cd77e9
Tanya Dewland <tanya.dewland@gmail.com>
2024-09-03 11:27:09 +0200

    Modify Navbar to use the NavItem component, pass the props

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -1,32 +1,23 @@
-"use client"
-import classNames from 'classnames';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link'
-
-import styles from './Navbar.module.css';
+"use client";
+import { usePathname } from "next/navigation";
+import NavItem from "./NavItem";
+import styles from "./Navbar.module.css";
 
 const navbarItems = [
-  {
-    title: 'ABOUT US',
-    link: '/about_us',
-  },
-  {
-    title: 'DESTINATION',
-    link: '/destination',
-  },
-  {
-    title: 'NASA COLLABORATION',
-    link: '/nasa_collaboration',
-  }
+  { id: 1, title: "ABOUT US", link: "/about_us" },
+  { id: 2, title: "DESTINATION", link: "/destination" },
+  { id: 3, title: "NASA COLLABORATION", link: "/nasa_collaboration" },
 ];
 
 export const Navbar = () => {
-  const currentPath = usePathname()
+  const currentPath = usePathname();
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.navbarLogo}>
-        <a href="/"><img src="/shared/logo.svg" alt="" /> GALACTICA</a>
+        <a href="/">
+          <img src="/shared/logo.svg" alt="" /> GALACTICA
+        </a>
       </div>
       <div className={styles.decorativeLine} />
       <nav className={styles.navbar}>
@@ -35,25 +26,19 @@ export const Navbar = () => {
           {/* TASK - React 1 week 2 */}
           {/* Create a <NavItem> component, which accepts the following:  */}
           {/* title, link, isActive  */}
-          <li className={classNames(styles.navbarLinks, {
-            [styles.isLinkActive]: navbarItems[0].link === currentPath,
-          })}>
-            <Link href={navbarItems[0].link}><b>01</b> {navbarItems[0].title}</Link>
-          </li>
-          <li className={classNames(styles.navbarLinks, {
-            [styles.isLinkActive]: navbarItems[1].link === currentPath,
-          })}>
-            <Link href={navbarItems[1].link}><b>02</b> {navbarItems[1].title}</Link>
-          </li>
-          <li className={classNames(styles.navbarLinks, {
-            [styles.isLinkActive]: navbarItems[2].link === currentPath,
-          })}>
-            <Link href={navbarItems[2].link}><b>03</b> NASA COLLABORATION</Link>
-          </li>
+          {navbarItems.map((item, index) => (
+            <NavItem
+              key={item.id}
+              title={item.title}
+              link={item.link}
+              isActive={item.link === currentPath}
+              index={index}
+            />
+          ))}
           {/* TASK - React 1 week 3 */}
           {/* replace repeating content by using navbarItems.map(() => <NavLink />) */}
         </ul>
       </nav>
     </header>
   );
-}
+};
