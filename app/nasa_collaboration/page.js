64286aea91d18221351171cc0bc16b3fac3cb382
Tanya Dewland <tanya.dewland@gmail.com>
2024-09-10 12:19:47 +0200

    Add API key

--- app/nasa_collaboration/page.js
+++ app/nasa_collaboration/page.js
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 
 // Read "/app/nasa_collaboration/README.md" for more info about the API_KEY
 // You need a proper API_KEY for the requests to work
-const API_KEY = "API_KEY";
+const API_KEY = "hOAFl6qsV6ipmn62jBxxfQrFhbmuW9JMiVzuh4RA";
 
 const NASA_URLs = {
   astronomyPicOfTheDay: `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
